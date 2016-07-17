72159e3862e2c2df2c05263cef636c9221d159bb
Martin Brandl <mjibrandl@gmail.com>
2016-07-17 19:06:47 +0200

    Added message controller

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace WebAPI
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // convention based routing:
+
             //config.Routes.MapHttpRoute(
             //    name: "DefaultApi",
             //    routeTemplate: "api/{controller}/{id}",
