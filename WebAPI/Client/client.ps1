954ad47938d766147f951e4f4396eadca0e57cae
Martin Brandl <mjibrandl@gmail.com>
2016-07-18 15:33:18 +0200

    Added some Controller methods

--- WebAPI/Client/client.ps1
+++ WebAPI/Client/client.ps1
@@ -1,1 +1,6 @@
-Invoke-WebRequest -Uri 'https://localhost/WebAPI/api/default'
+$x = [guid]::NewGuid()
+
+Invoke-WebRequest -Uri "https://localhost/WebAPI/api/default/$x/helloFred" -Method Post
+
+#Invoke-WebRequest -Uri 'https://localhost/WebAPI/api/default' | select StatusCode, Content
+#Invoke-WebRequest -Uri 'https://localhost/WebAPI/api/default/hello, world' | select StatusCode, Content
--- WebAPI/Controllers/MessageController.cs
+++ WebAPI/Controllers/MessageController.cs
@@ -1,19 +1,73 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace WebAPI.Controllers
 {
     [RoutePrefix("api/default")]
     public class MessageController : ApiController
     {
-        [Route("")]
-        public string GetMessage()
+        private static readonly IDictionary<Guid, string> mMessages = new ConcurrentDictionary<Guid, string>();
+
+        [ResponseType(typeof(string))]
+        [Route("{id}", Name="GetMessage")]
+        public async Task<IHttpActionResult> GetMessageAsync(Guid id)
         {
-            return "hello world!";
+            string retrievedMessage;
+            
+            if (!mMessages.TryGetValue(id, out retrievedMessage))
+            {
+                return NotFound();
+            }
+
+            return Ok(retrievedMessage);
         }
+
+        [ResponseType(typeof(IDictionary<Guid, string>))]
+        [Route("", Name = "GetMessages")]
+        public async Task<IHttpActionResult> GetMessages()
+        {
+            return Ok(mMessages);
+        }
+
+
+        [ResponseType(typeof(string))]
+        [Route("{id}/{message}", Name = "AddMessage")]
+        [HttpPost]
+        public async Task<IHttpActionResult> AddMessageAsync(Guid id, string message)
+        {
+            if (Guid.Empty.Equals(id))
+            {
+                return BadRequest("Invalid id.");
+            }
+
+            if (mMessages.ContainsKey(id))
+            {
+                return Conflict();
+            }
+            
+            mMessages.Add(id, message);
+
+            return CreatedAtRoute("GetMessage", new {id = id}, message);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult DeleteCustomer(Guid id)
+        {
+            if (!mMessages.Remove(id))
+            {
+                return NotFound();
+            }
+            
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
     }
 }
