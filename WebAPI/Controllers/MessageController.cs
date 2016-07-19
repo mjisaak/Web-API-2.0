65abed7cb74b4ca54a07e115fa6bddff49d25722
Martin Brandl <mjibrandl@gmail.com>
2016-07-19 10:06:27 +0200

    Added swagger and enabled XML documentation

--- WebAPI/Controllers/MessageController.cs
+++ WebAPI/Controllers/MessageController.cs
@@ -10,11 +10,23 @@ using System.Web.Http.Description;
 
 namespace WebAPI.Controllers
 {
+    /// <summary>
+    /// Message resource controller
+    /// </summary>
     [RoutePrefix("api/default")]
     public class MessageController : ApiController
     {
         private static readonly IDictionary<Guid, string> mMessages = new ConcurrentDictionary<Guid, string>();
 
+        /// <summary>
+        /// Gets the message with the specified id.
+        /// </summary>
+        /// <param name="id">The id of the message to get.</param>
+        /// <returns>The message related to the specified id.</returns>
+        /// <remarks>Currently, only one message can be retrieved at once.</remarks>
+        /// <response code="200">OK</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server Error</response>
         [ResponseType(typeof(string))]
         [Route("{id}", Name="GetMessage")]
         public async Task<IHttpActionResult> GetMessageAsync(Guid id)
@@ -29,16 +41,31 @@ namespace WebAPI.Controllers
             return Ok(retrievedMessage);
         }
 
+        /// <summary>
+        /// Gets all messages.
+        /// </summary>
+        /// <returns>A dictionary of id, messages.</returns>
+        /// <response code="200">OK</response>
+        /// <response code="500">Internal Server Error</response>
         [ResponseType(typeof(IDictionary<Guid, string>))]
         [Route("", Name = "GetMessages")]
         public async Task<IHttpActionResult> GetMessages()
         {
             return Ok(mMessages);
         }
-
-
+        
+        /// <summary>
+        /// Adds a message and returns the new resource location.
+        /// </summary>
+        /// <param name="id">The id of the message to add.</param>
+        /// <param name="message">The message.</param>
+        /// <returns>The route of the added message.</returns>
+        /// <response code="201">Created</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="409">Conflict</response>
+        /// <response code="500">Internal Server Error</response>
         [ResponseType(typeof(string))]
-        [Route("{id}/{message}", Name = "AddMessage")]
+        [Route("{id:guid}/{message}", Name = "AddMessage")]
         [HttpPost]
         public async Task<IHttpActionResult> AddMessageAsync(Guid id, string message)
         {
@@ -57,8 +84,15 @@ namespace WebAPI.Controllers
             return CreatedAtRoute("GetMessage", new {id = id}, message);
         }
 
+        /// <summary>
+        /// Deletes the message with the correlated id.
+        /// </summary>
+        /// <param name="id">The id of the message to delete.</param>
+        /// <response code="204">No Content</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server Error</response>
         [Route("{id}")]
-        public IHttpActionResult DeleteCustomer(Guid id)
+        public IHttpActionResult DeleteMessage(Guid id)
         {
             if (!mMessages.Remove(id))
             {
@@ -67,7 +101,5 @@ namespace WebAPI.Controllers
             
             return StatusCode(HttpStatusCode.NoContent);
         }
-
-
     }
 }
